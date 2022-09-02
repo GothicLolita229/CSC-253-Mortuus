9aceef60c91b6d13b2fd4b2a59b0a33a2e4e18d9
Lourdes Linares <linaresl1836@ad.faytechcc.edu>
2022-09-02 19:05:59 -0400

    Load player and added inheritance to potions, treasure, and item

--- MortuusClassLibrary/Treasure.cs
+++ MortuusClassLibrary/Treasure.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace MortuusClassLibrary
 {
-    class Treasure
+    class Treasure : IngameItem
     {
+        private string _idNumber;
+        private string _name;
+        private string _description;
+        private string _questItem;
+        private string _price;
+
+        public Treasure(string idNumber, string name, string description, string questItem, string price)
+            : base(idNumber, name, description, price)
+        {
+            _idNumber = idNumber;
+            _name = name;
+            _description = description;
+            _questItem = questItem;
+            _price = price;
+        }
+
+        public string QuestItem { get; set; }
     }
 }
