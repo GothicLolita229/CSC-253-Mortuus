8e10258fc0096c915df79921ef1a697b246c5377
Lourdes Linares <linaresl1836@ad.faytechcc.edu>
2022-09-02 19:12:08 -0400

    Weapon class inhertance added

--- MortuusClassLibrary/Weapon.cs
+++ MortuusClassLibrary/Weapon.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace MortuusClassLibrary
 {
-    public class Weapon
+    public class Weapon : IngameItem
     {
+        private string _idNumber;
+        private string _name;
+        private string _description;
+        private string _damageType;
+        private int _damage;
+        private string _price;
+
+        public Weapon(string idNumber, string name, string description, string damageType, int damage, string price)
+            : base(idNumber, name, description, price)
+        {
+            _idNumber = idNumber;
+            _name = name;
+            _description = description;
+            _damageType = damageType;
+            _damage = damage;
+            _price = price;
+        }
+
+
+        public string DamageType { get; set; }
+        public string Damage { get; set; }
     }
 }
