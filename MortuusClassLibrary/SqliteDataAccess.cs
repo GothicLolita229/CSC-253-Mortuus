29861f3faa9692e018fd160ad85028e1b7cb74a0
GothicLolita229 <90853197+GothicLolita229@users.noreply.github.com>
2022-09-19 11:42:45 -0400

    Add files via upload
    Added Sqlite extension

---
+++ MortuusClassLibrary/SqliteDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortuusClassLibrary
+{
+    public class SqliteDataAccess
+    {
+        public static List<Room> LoadRooms()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<Room>("SELECT * FROM RoomsTable", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
