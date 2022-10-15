2ee6124b8a1bc9c482e0d29a4cfaffe74b66685a
GothicLolita229 <ceciliathomas4216@gmail.com>
2022-10-15 09:39:14 -0400

    Added a lambda
    In GameMenuSwitcher, changed all writelines to take from Lambda expression that prints out a string

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -19,11 +19,7 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             Room.RoomDisplay();
-            //Room.RoomExitDisplay(); //Change Exits to ints that display roomID numbers and then use those
-                                    // with a switch statement to move to different rooms. For example 
-                                    // The Foyer has exits (0, 1, 3, 5) where 0 is north and there is no room
-                                    // 1 is back to the entrance, 3 is the Great Hall and, 5 is store room.
-                                    // Or make a map with only two exits per room like Mr. Norris suggested
+            
             Game();
         }
 
