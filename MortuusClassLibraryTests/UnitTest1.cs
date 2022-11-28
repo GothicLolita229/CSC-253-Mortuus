45590805e8271e3ff31fb9a01b6f80d93a2e69a1
CiaraM100104 <90720471+CiaraM100104@users.noreply.github.com>
2022-11-28 08:18:55 -0500

    Updated Unit Tests
    The packages are proving to be an issue but I will fix them when I can get to Visual Studios 2022 back home.

---
+++ MortuusClassLibraryTests/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MortuusClassLibrary;
+
+namespace MortuusClassLibraryTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LoadRoomTest()
+        {
+            Room room = SqliteDataAccess.LoadRoom(301);
+            Assert.AreEqual(301, room.ID);
+        }
+
+        public void LoadTreasureTest()
+        {
+            Treasure treasure = SqliteDataAccess.LoadTreasure(601);
+            Assert.AreEqual(601, treasure.IDNumber);
+        }
+
+        public void LoadWeaponTest()
+        {
+            Weapon weapon = SqliteDataAccess.LoadWeapon(701);
+            Assert.AreEqual(701, weapon.IDNumber);
+        }
+
+        public void LoadPotionTest()
+        {
+            Potion potion = SqliteDataAccess.LoadPotion(401);
+            Assert.AreEqual(401, potion.IDNumber);
+        }
+
+        public void LoadMobTest()
+        {
+            Mob mob = SqliteDataAccess.LoadMob(201);
+            Assert.AreEqual(201, mob.ID);
+        }
+    }
+}
