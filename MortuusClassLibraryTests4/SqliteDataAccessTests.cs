0577c7ab33da6fd2ed5757edb0d56d5efd75bd08
CiaraM100104 <90720471+CiaraM100104@users.noreply.github.com>
2022-11-28 02:38:13 -0500

    Tried to add in some unit tests
    Each test checks if the objects load in properly.

---
+++ MortuusClassLibraryTests4/SqliteDataAccessTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MortuusClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortuusClassLibrary.Tests
+{
+    [TestClass()]
+    public class SqliteDataAccessTests
+    {
+        [TestMethod()]
+        public void LoadTreasureTest()
+        {
+            Room room = SqliteDataAccess.LoadRoom(301);
+            Assert.AreEqual(301, room.ID);
+        }
+
+    }
+}
